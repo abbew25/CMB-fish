fixed some small bugs (issue with using copy instead of deepcopy and issue with swapping derivatives around)

src/CMB_forecast_main.py
@@ -7,6 +7,7 @@ from fishercomputations import (
 )
 from setup import CosmoResults, write_fisher
 from rich.console import Console
+import copy
 
 if __name__ == "__main__":
     console = Console()
@@ -18,12 +19,12 @@ if __name__ == "__main__":
     if "geff_fixed" not in pardict:
         pardict["geff_fixed"] = True
 
-    fracstepthetastar = 0.01
-    fracstepomegab = 0.01
-    fracstepomegacdm = 0.01
-    fracstepAs = 0.01
-    fracstepns = 0.01
-    fracsteptau = 0.01
+    fracstepthetastar = 0.0001
+    fracstepomegab = 0.0001
+    fracstepomegacdm = 0.0001
+    fracstepAs = 0.0001
+    fracstepns = 0.0001
+    fracsteptau = 0.0001
 
     # Set up the linear power spectrum and derived parameters based on the input cosmology
     cosmo = CosmoResults(
@@ -52,20 +53,14 @@ if __name__ == "__main__":
         fracstepns=fracstepns,
         fracsteptau=fracsteptau,
     )
-    derClthetastarTT, derClthetastarEE, derClthetastarTE = derivatives[0]
 
-    derClATT, derClAEE, derClATE = derivatives[1]
-
+    derClATT, derClAEE, derClATE = derivatives[0]
+    derClthetastarTT, derClthetastarEE, derClthetastarTE = derivatives[1]
     derClOmegabTT, derClOmegabEE, derClOmegabTE = derivatives[2]
-
     derClOmegacdmTT, derClOmegacdmEE, derClOmegacdmTE = derivatives[3]
-
     derClAsTT, derClAsEE, derClAsTE = derivatives[4]
-
     derClnsTT, derClnsEE, derClnsTE = derivatives[5]
-
     derCltauTT, derCltauEE, derCltauTE = derivatives[6]
-
     derClgeff = []
     if not pardict.as_bool("geff_fixed"):
         derClgeffTT, derClgeffEE, derClgeffTE = derivatives[7]
@@ -97,18 +92,16 @@ if __name__ == "__main__":
 
     if pardict.as_bool("geff_fixed"):
         console.log(
-            "#  100theta_star  100theta_star_err(%)  A(Neff)  A(Neff)_err(%)  Omegab   Omegab_err(%)  Omegacdm  Omegacdm_err(%)  lnAs10  lnAs10_err(%)  ns  ns_err(%)  tau  tau_err(%)"
+            "#  100theta_star  100theta_star_err(%)  A(Neff)  A(Neff)_err(%)  Omegab   Omegab_err(%)  Omegacdm  Omegacdm_err(%)  As10  As10_err(%)  ns  ns_err(%)  tau  tau_err(%)"
         )
 
     else:
         console.log(
-            "#  100theta_star  100theta_star_err(%)  A(Neff)  A(Neff)_err(%)  Omegab   Omegab_err(%)  Omegacdm  Omegacdm_err(%)  lnAs10  lnAs10_err(%)  ns  ns_err(%)  tau  tau_err(%)  log10Geff  geff_err(%)"
+            "#  100theta_star  100theta_star_err(%)  A(Neff)  A(Neff)_err(%)  Omegab   Omegab_err(%)  Omegacdm  Omegacdm_err(%)  As10  As10_err(%)  ns  ns_err(%)  tau  tau_err(%)  log10Geff  geff_err(%)"
         )
 
     Catch = Fish(
         cosmo,
-        float(pardict["lmin"]),
-        float(pardict["lmax"]),
         [derClthetastarTT, derClthetastarEE, derClthetastarTE],
         [derClATT, derClAEE, derClATE],
         [derClOmegabTT, derClOmegabEE, derClOmegabTE],
@@ -120,6 +113,9 @@ if __name__ == "__main__":
         pardict.as_bool("geff_fixed"),
     )
 
+    # add prior on tau
+    Catch[-1][-1] += 1.0 / (0.01**2)
+
     cov = np.linalg.inv(Catch)
     errs = np.sqrt(np.diag(cov))
     means = np.array(
@@ -128,7 +124,7 @@ if __name__ == "__main__":
             cosmo.A_phi,
             cosmo.Omegab,
             cosmo.Omega_cdm,
-            cosmo.lnAs10,
+            np.power(cosmo.lnAs10, 10.0),
             cosmo.ns,
             cosmo.tau,
             cosmo.log10Geff,
@@ -141,7 +137,7 @@ if __name__ == "__main__":
                 cosmo.A_phi,
                 cosmo.Omegab,
                 cosmo.Omega_cdm,
-                cosmo.lnAs10,
+                np.power(cosmo.lnAs10, 10.0),
                 cosmo.ns,
                 cosmo.tau,
             ]
@@ -193,6 +189,27 @@ if __name__ == "__main__":
         means,
     )
 
+    Catch_standard = copy.deepcopy(Catch)
+    Catch_standard = np.delete(Catch_standard, 1, axis=0)
+    Catch_standard = np.delete(Catch_standard, 1, axis=1)
+
+    print(np.sqrt(np.diag(np.linalg.inv(Catch_standard))))
+
+    # console.log(
+    #     Catch
+    # )
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(Catch)
+
+    # plt.show()
+
+    # for i in range(len(Catch)):
+    #     for j in range(len(Catch)):
+    #         if i < j:
+    #             print(i, j)
+    #             print(Catch[i,j] - Catch[j,i])
+
     alpha_nu = 8.0 / 7.0 * (11.0 / 4.0) ** (4.0 / 3.0)
     eps_neff1 = 1.0 / (1.0 + alpha_nu)
     eps_neffstandard = 3.044 / (3.044 + alpha_nu)
@@ -226,9 +243,9 @@ if __name__ == "__main__":
                 columns=[
                     r"$100\theta_*$",
                     r"$A_{\phi}$",
-                    r"$\Omega_b$",
-                    r"$\Omega_{\mathrm{cdm}}$",
-                    r"$ln(A_s10^{10})$",
+                    r"$\Omega_bh^2$",
+                    r"$\Omega_{\mathrm{cdm}}h^2$",
+                    r"$(A_s10^{10})$",
                     r"$n_s$",
                     r"$\tau$",
                 ],
@@ -252,9 +269,9 @@ if __name__ == "__main__":
                 columns=[
                     r"$100\theta_*$",
                     r"$A_{\phi}$",
-                    r"$\Omega_b$",
-                    r"$\Omega_{\mathrm{cdm}}$",
-                    r"$ln(A_s10^{10})$",
+                    r"$\Omega_b$h^2",
+                    r"$\Omega_{\mathrm{cdm}}h^2$",
+                    r"$(A_s10^{10})$",
                     r"$n_s$",
                     r"$\tau$",
                     r"$\log_{10}G_{\mathrm{eff}}$",

src/fishercomputations.py
@@ -102,7 +102,7 @@ def compute_deriv_omegacdm(cosmo: CosmoResults, fracstep: float = 0.002):
 def compute_deriv_As(cosmo: CosmoResults, fracstep: float = 0.002):
     cl_before = splev(cosmo.ell, cosmo.clTT_minAs)
     cl_after = splev(cosmo.ell, cosmo.clTT_plusAs)
-    deltaAs = fracstep * cosmo.lnAs10
+    deltaAs = fracstep * np.power(cosmo.lnAs10, 10.0)
     derCl_As = (cl_after - cl_before) / (2.0 * deltaAs)
     derCl_As_interpTT = interp1d(cosmo.ell, derCl_As)
 
@@ -159,8 +159,7 @@ def compute_deriv_tau(cosmo: CosmoResults, fracstep: float = 0.002):
     return derCl_tau_interpTT, derCl_tau_interpEE, derCl_tau_interpTE
 
 
-def compute_deriv_phiamplitude(cosmo: CosmoResults):
-    dl = 0.01
+def compute_deriv_phiamplitude(cosmo: CosmoResults, dl: float = 0.01):
     clTT = splev(cosmo.ell, cosmo.clTT)
     clEE = splev(cosmo.ell, cosmo.clEE)
     clTE = splev(cosmo.ell, cosmo.clTE)
@@ -223,8 +222,6 @@ def compute_deriv_phiamplitude(cosmo: CosmoResults):
 
 def Fish(
     cosmo: CosmoResults,
-    lmin: float,
-    lmax: float,
     derClthetastar: list,
     derClbeta: list,
     derClOmegab: list,
@@ -251,7 +248,10 @@ def Fish(
     """
     # Uses Simpson's rule or adaptive quadrature to integrate over all k and mu.
     # mu and k values for Simpson's rule
-    lvec = np.arange(lmin, lmax)
+    lvec = np.arange(
+        np.min([cosmo.lminTT, cosmo.lminTE, cosmo.lminEE]),
+        np.max([cosmo.lmaxTT, cosmo.lmaxTE, cosmo.lmaxEE]),
+    )
 
     # 2D integration
     ManyFish = simps(
@@ -382,6 +382,13 @@ def CastNet(
             ]
         )
 
+        if lval < cosmo.lminTT or lval > cosmo.lmaxTT:
+            derCl[:, 0] = 0.0
+        if lval < cosmo.lminEE or lval > cosmo.lmaxEE:
+            derCl[:, 1] = 0.0
+        if lval < cosmo.lminTE or lval > cosmo.lmaxTE:
+            derCl[:, 2] = 0.0
+
         if not geff_fixed:
             derCl = np.vstack(
                 (derCl, ([derClgeffval[0][i], derClgeffval[1][i], derClgeffval[2][i]]))
@@ -419,18 +426,19 @@ def compute_inv_cov(
     cov_inv: np.ndarray
     """
 
-    deltab = np.array([33, 23, 14, 10, 7, 5, 5]) / 3437.75
+    deltabT = np.array([33, 23, 14, 10, 7, 5, 5]) / 3437.75
+    deltabE = np.array([14, 10, 7, 5, 5]) / 3437.75
     deltaT = np.array([145, 149, 137, 65, 43, 66, 200]) / 3437.75
     deltaE = np.array([450.0, 103.0, 81.0, 134.0, 406.0]) / 3437.75
     Nl_freqT = (
         1.0
         / (deltaT**2)
-        * np.exp(-1.0 * lval * (lval + 1.0) * deltab**2 / (8.0 * np.log(2.0)))
+        * np.exp(-1.0 * lval * (lval + 1.0) * deltabT**2 / (8.0 * np.log(2.0)))
     )
     Nl_freqE = (
         1.0
         / (deltaE**2)
-        * np.exp(-1.0 * lval * (lval + 1.0) * deltab**2 / (8.0 * np.log(2.0)))
+        * np.exp(-1.0 * lval * (lval + 1.0) * deltabE**2 / (8.0 * np.log(2.0)))
     )
     Nl_DeltaT = 1.0 / np.sum(Nl_freqT)
     Nl_DeltaE = 1.0 / np.sum(Nl_freqE)

src/setup.py
@@ -32,8 +32,8 @@ class CosmoResults:
             self.area,
         ) = self.run_camb(pardict)
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
         pardictbefore["thetastar"] = self.theta_star * (1.0 - fracstepthetastar) / 100.0
         del pardictbefore["h"]
         pardictafter["thetastar"] = self.theta_star * (1.0 + fracstepthetastar) / 100.0
@@ -49,8 +49,8 @@ class CosmoResults:
         self.clEE_plusthetastar = self.plus_thstar[2]
         self.clTE_plusthetastar = self.plus_thstar[3]
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
         pardictbefore["omega_b"] = self.Omegab * (1.0 - fracstepomegab)
         pardictafter["omega_b"] = self.Omegab * (1.0 + fracstepomegab)
 
@@ -63,8 +63,8 @@ class CosmoResults:
         self.clEE_plusOmegab = self.plus_Omegab[2]
         self.clTE_plusOmegab = self.plus_Omegab[3]
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
         pardictbefore["omega_cdm"] = self.Omega_cdm * (1.0 - fracstepomegacdm)
         pardictafter["omega_cdm"] = self.Omega_cdm * (1.0 + fracstepomegacdm)
 
@@ -77,10 +77,12 @@ class CosmoResults:
         self.clEE_plusOmegacdm = self.plus_Omegacdm[2]
         self.clTE_plusOmegacdm = self.plus_Omegacdm[3]
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
-        pardictbefore["A_s"] = np.exp(self.lnAs10 * (1.0 - fracstepAs)) * 1.0e-10
-        pardictafter["A_s"] = np.exp(self.lnAs10 * (1.0 + fracstepAs)) * 1.0e-10
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
+        pardictbefore["A_s"] = (
+            np.power(self.lnAs10, 10.0) * (1.0 - fracstepAs) * 1.0e-10
+        )
+        pardictafter["A_s"] = np.power(self.lnAs10, 10.0) * (1.0 + fracstepAs) * 1.0e-10
         self.minus_As = self.run_camb(pardictbefore)
         self.clTT_minAs = self.minus_As[1]
         self.clEE_minAs = self.minus_As[2]
@@ -90,8 +92,8 @@ class CosmoResults:
         self.clEE_plusAs = self.plus_As[2]
         self.clTE_plusAs = self.plus_As[3]
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
         pardictbefore["n_s"] = self.ns * (1.0 - fracstepns)
         pardictafter["n_s"] = self.ns * (1.0 + fracstepns)
         self.minus_ns = self.run_camb(pardictbefore)
@@ -103,8 +105,8 @@ class CosmoResults:
         self.clEE_plusns = self.plus_ns[2]
         self.clTE_plusns = self.plus_ns[3]
 
-        pardictbefore = pardict.copy()
-        pardictafter = pardict.copy()
+        pardictbefore = copy.deepcopy(pardict)
+        pardictafter = copy.deepcopy(pardict)
         pardictbefore["tau_reio"] = float(pardict["tau_reio"]) * (1.0 - fracsteptau)
         pardictafter["tau_reio"] = float(pardict["tau_reio"]) * (1.0 + fracsteptau)
         self.minus_tau = self.run_camb(pardictbefore)