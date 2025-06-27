fixed some stupid bugs in the covariance matrix and improved robustness of derivatives

src/CMB_forecast_main.py
@@ -18,12 +18,19 @@ if __name__ == "__main__":
     if "geff_fixed" not in pardict:
         pardict["geff_fixed"] = True
 
-    fracstepthetastar = 0.02
-    fracstepomegab = 0.02
-    fracstepomegacdm = 0.02
-    fracstepAs = 0.02
-    fracstepns = 0.02
-    fracsteptau = 0.02
+    # fracstepthetastar = 0.0001  # good
+    # fracstepomegab = 0.0005
+    # fracstepomegacdm = 0.0001
+    # fracstepAs = 0.0001  # good
+    # fracstepns = 0.00001  # good
+    # fracsteptau = 0.0001  # good
+
+    fracstepthetastar = 0.001
+    fracstepomegab = 0.001
+    fracstepomegacdm = 0.001
+    fracstepAs = 0.001
+    fracstepns = 0.0005
+    fracsteptau = 0.0001
 
     # Set up the linear power spectrum and derived parameters based on the input cosmology
     cosmo = CosmoResults(
@@ -122,16 +129,25 @@ if __name__ == "__main__":
     # Catch[-1][-1] += 1.0 / (0.01**2)
 
     # cov = np.linalg.inv(Catch)
-    # print(np.linalg.cond(Catch))
+    # console.log('matrix condition number: ', np.linalg.cond(Catch))
+
+    # import copy
+    # Catch2 = copy.deepcopy(Catch)
+    # Catch2 = np.delete(Catch2, 1, axis=0)
+    # Catch2 = np.delete(Catch2, 1, axis=1)
+
+    # console.log('matrix condition number without A_phi: ', np.linalg.cond(Catch2))
+
+    # exit()
 
     # import matplotlib.pyplot as plt
     # plt.imshow(np.log10(np.abs(Catch)), cmap="viridis")
     # plt.colorbar()
     # plt.show()
 
-    # Catch = np.delete(Catch, 1, axis=0)
-    # Catch = np.delete(Catch, 1, axis=1)
-
+    Catch = np.delete(Catch, 1, axis=0)  # remove A_phi
+    Catch = np.delete(Catch, 1, axis=1)
+    # print(Catch)
     # for i in range(len(Catch)):
     #     for j in range(len(Catch)):
     #         if i < j:
@@ -147,8 +163,8 @@ if __name__ == "__main__":
     means = np.array(
         [
             cosmo.theta_star,
-            cosmo.A_phi,
-            cosmo.Omegab * 100.0,
+            # cosmo.A_phi,
+            cosmo.Omegab,
             cosmo.Omega_cdm,
             cosmo.lnAs10,
             cosmo.ns,
@@ -160,8 +176,8 @@ if __name__ == "__main__":
         means = np.array(
             [
                 cosmo.theta_star,
-                cosmo.A_phi,
-                cosmo.Omegab * 100.0,
+                # cosmo.A_phi,
+                cosmo.Omegab,
                 cosmo.Omega_cdm,
                 cosmo.lnAs10,
                 cosmo.ns,
@@ -170,8 +186,8 @@ if __name__ == "__main__":
         )
 
     if not pardict.as_bool("geff_fixed"):
-        txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
-            # txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
+        # txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
+        txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
             means[0],
             errs[0] / means[0] * 100.0,
             means[1],
@@ -186,12 +202,12 @@ if __name__ == "__main__":
             errs[5] / means[5] * 100.0,
             means[6],
             errs[6] / means[6] * 100.0,
-            means[7],
-            errs[7] / means[7] * 100.0,
+            # means[7],
+            # errs[7] / means[7] * 100.0,
         )
     else:
-        txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
-            # txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
+        # txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
+        txt = "{:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.5f}    {:.5f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}    {:.3f}".format(
             means[0],
             errs[0] / means[0] * 100.0,
             means[1],
@@ -204,8 +220,8 @@ if __name__ == "__main__":
             errs[4] / means[4] * 100.0,
             means[5],
             errs[5] / means[5] * 100.0,
-            means[6],
-            errs[6] / means[6] * 100.0,
+            # means[6],
+            # errs[6] / means[6] * 100.0,
         )
 
     console.log(txt)
@@ -216,6 +232,8 @@ if __name__ == "__main__":
         cov,
         means,
     )
+
+    exit()
 
     # Catch_standard = copy.deepcopy(Catch)
     # Catch_standard = np.delete(Catch_standard, 1, axis=0)
@@ -270,7 +288,7 @@ if __name__ == "__main__":
                 cov,
                 columns=[
                     r"$100\theta_*$",
-                    r"$A_{\phi}$",
+                    # r"$A_{\phi}$",
                     r"$100\Omega_bh^2$",
                     r"$\Omega_{\mathrm{cdm}}h^2$",
                     r"$\ln(A_s10^{10})$",
@@ -296,7 +314,7 @@ if __name__ == "__main__":
                 cov,
                 columns=[
                     r"$100\theta_*$",
-                    r"$A_{\phi}$",
+                    # r"$A_{\phi}$",
                     r"$100\Omega_b$h^2",
                     r"$\Omega_{\mathrm{cdm}}h^2$",
                     r"$\ln(A_s10^{10})$",

src/setup.py
@@ -32,89 +32,119 @@ class CosmoResults:
             self.area,
         ) = self.run_camb(pardict)
 
+        # for derivatives w.r.t. thetastar
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
         pardictbefore["thetastar"] = self.theta_star * (1.0 - fracstepthetastar) / 100.0
         del pardictbefore["h"]
         pardictafter["thetastar"] = self.theta_star * (1.0 + fracstepthetastar) / 100.0
         del pardictafter["h"]
+        minus_thstar = self.run_camb(pardictbefore)
+        plus_thstar = self.run_camb(pardictafter)
+        self.clTTEETE_minthetastar = minus_thstar[1:4]
+        self.clTTEETE_plusthetastar = plus_thstar[1:4]
 
-        self.minus_thstar = self.run_camb(pardictbefore)
-        self.clTT_minthetastar = self.minus_thstar[1]
-        self.clEE_minthetastar = self.minus_thstar[2]
-        self.clTE_minthetastar = self.minus_thstar[3]
+        pardictbefore["thetastar"] = (
+            self.theta_star * (1.0 - 2.0 * fracstepthetastar) / 100.0
+        )
+        pardictafter["thetastar"] = (
+            self.theta_star * (1.0 + 2.0 * fracstepthetastar) / 100.0
+        )
+        minus_thstar = self.run_camb(pardictbefore)
+        plus_thstar = self.run_camb(pardictafter)
+        self.clTTEETE_minthetastar2 = minus_thstar[1:4]
+        self.clTTEETE_plusthetastar2 = plus_thstar[1:4]
 
-        self.plus_thstar = self.run_camb(pardictafter)
-        self.clTT_plusthetastar = self.plus_thstar[1]
-        self.clEE_plusthetastar = self.plus_thstar[2]
-        self.clTE_plusthetastar = self.plus_thstar[3]
-
+        # for derivatives w.r.t. Omegab
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
         pardictbefore["omega_b"] = self.Omegab * (1.0 - fracstepomegab)
         pardictafter["omega_b"] = self.Omegab * (1.0 + fracstepomegab)
+        minus_Omegab = self.run_camb(pardictbefore)
+        self.clTTEETE_minOmegab = minus_Omegab[1:4]
+        plus_Omegab = self.run_camb(pardictafter)
+        self.clTTEETE_plusOmegab = plus_Omegab[1:4]
 
-        self.minus_Omegab = self.run_camb(pardictbefore)
-        self.clTT_minOmegab = self.minus_Omegab[1]
-        self.clEE_minOmegab = self.minus_Omegab[2]
-        self.clTE_minOmegab = self.minus_Omegab[3]
-        self.plus_Omegab = self.run_camb(pardictafter)
-        self.clTT_plusOmegab = self.plus_Omegab[1]
-        self.clEE_plusOmegab = self.plus_Omegab[2]
-        self.clTE_plusOmegab = self.plus_Omegab[3]
+        pardictbefore["omega_b"] = self.Omegab * (1.0 - 2.0 * fracstepomegab)
+        pardictafter["omega_b"] = self.Omegab * (1.0 + 2.0 * fracstepomegab)
+        minus_Omegab = self.run_camb(pardictbefore)
+        self.clTTEETE_minOmegab2 = minus_Omegab[1:4]
+        plus_Omegab = self.run_camb(pardictafter)
+        self.clTTEETE_plusOmegab2 = plus_Omegab[1:4]
 
+        # for derivatives w.r.t. Omega_cdm
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
         pardictbefore["omega_cdm"] = self.Omega_cdm * (1.0 - fracstepomegacdm)
         pardictafter["omega_cdm"] = self.Omega_cdm * (1.0 + fracstepomegacdm)
+        minus_Omegacdm = self.run_camb(pardictbefore)
+        self.clTTEETE_minOmegacdm = minus_Omegacdm[1:4]
+        plus_Omegacdm = self.run_camb(pardictafter)
+        self.clTTEETE_plusOmegacdm = plus_Omegacdm[1:4]
 
-        self.minus_Omegacdm = self.run_camb(pardictbefore)
-        self.clTT_minOmegacdm = self.minus_Omegacdm[1]
-        self.clEE_minOmegacdm = self.minus_Omegacdm[2]
-        self.clTE_minOmegacdm = self.minus_Omegacdm[3]
-        self.plus_Omegacdm = self.run_camb(pardictafter)
-        self.clTT_plusOmegacdm = self.plus_Omegacdm[1]
-        self.clEE_plusOmegacdm = self.plus_Omegacdm[2]
-        self.clTE_plusOmegacdm = self.plus_Omegacdm[3]
+        pardictbefore["omega_cdm"] = self.Omega_cdm * (1.0 - 2.0 * fracstepomegacdm)
+        pardictafter["omega_cdm"] = self.Omega_cdm * (1.0 + 2.0 * fracstepomegacdm)
+        minus_Omegacdm = self.run_camb(pardictbefore)
+        self.clTTEETE_minOmegacdm2 = minus_Omegacdm[1:4]
+        plus_Omegacdm = self.run_camb(pardictafter)
+        self.clTTEETE_plusOmegacdm2 = plus_Omegacdm[1:4]
 
+        # for derivatives w.r.t. A_s
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
+        del pardictbefore["ln10^{10}A_s"]
+        del pardictafter["ln10^{10}A_s"]
         pardictbefore["A_s"] = np.exp(self.lnAs10) * (1.0 - fracstepAs) * 1.0e-10
         pardictafter["A_s"] = np.exp(self.lnAs10) * (1.0 + fracstepAs) * 1.0e-10
-        self.minus_As = self.run_camb(pardictbefore)
-        self.clTT_minAs = self.minus_As[1]
-        self.clEE_minAs = self.minus_As[2]
-        self.clTE_minAs = self.minus_As[3]
-        self.plus_As = self.run_camb(pardictafter)
-        self.clTT_plusAs = self.plus_As[1]
-        self.clEE_plusAs = self.plus_As[2]
-        self.clTE_plusAs = self.plus_As[3]
+        minus_As = self.run_camb(pardictbefore)
+        self.clTTEETE_minAs = minus_As[1:4]
+        plus_As = self.run_camb(pardictafter)
+        self.clTTEETE_plusAs = plus_As[1:4]
 
+        pardictbefore["A_s"] = np.exp(self.lnAs10) * (1.0 - 2.0 * fracstepAs) * 1.0e-10
+        pardictafter["A_s"] = np.exp(self.lnAs10) * (1.0 + 2.0 * fracstepAs) * 1.0e-10
+        minus_As = self.run_camb(pardictbefore)
+        self.clTTEETE_minAs2 = minus_As[1:4]
+        plus_As = self.run_camb(pardictafter)
+        self.clTTEETE_plusAs2 = plus_As[1:4]
+
+        # for derivatives w.r.t. n_s
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
         pardictbefore["n_s"] = self.ns * (1.0 - fracstepns)
         pardictafter["n_s"] = self.ns * (1.0 + fracstepns)
-        self.minus_ns = self.run_camb(pardictbefore)
-        self.clTT_minns = self.minus_ns[1]
-        self.clEE_minns = self.minus_ns[2]
-        self.clTE_minns = self.minus_ns[3]
-        self.plus_ns = self.run_camb(pardictafter)
-        self.clTT_plusns = self.plus_ns[1]
-        self.clEE_plusns = self.plus_ns[2]
-        self.clTE_plusns = self.plus_ns[3]
+        minus_ns = self.run_camb(pardictbefore)
+        self.clTTEETE_minns = minus_ns[1:4]
+        plus_ns = self.run_camb(pardictafter)
+        self.clTTEETE_plusns = plus_ns[1:4]
 
+        pardictbefore["n_s"] = self.ns * (1.0 - 2.0 * fracstepns)
+        pardictafter["n_s"] = self.ns * (1.0 + 2.0 * fracstepns)
+        minus_ns = self.run_camb(pardictbefore)
+        self.clTTEETE_minns2 = minus_ns[1:4]
+        plus_ns = self.run_camb(pardictafter)
+        self.clTTEETE_plusns2 = plus_ns[1:4]
+
+        # for derivatives w.r.t. tau_reio
         pardictbefore = copy.deepcopy(pardict)
         pardictafter = copy.deepcopy(pardict)
         pardictbefore["tau_reio"] = float(pardict["tau_reio"]) * (1.0 - fracsteptau)
         pardictafter["tau_reio"] = float(pardict["tau_reio"]) * (1.0 + fracsteptau)
-        self.minus_tau = self.run_camb(pardictbefore)
-        self.clTT_mintau = self.minus_tau[1]
-        self.clEE_mintau = self.minus_tau[2]
-        self.clTE_mintau = self.minus_tau[3]
-        self.plus_tau = self.run_camb(pardictafter)
-        self.clTT_plustau = self.plus_tau[1]
-        self.clEE_plustau = self.plus_tau[2]
-        self.clTE_plustau = self.plus_tau[3]
+        minus_tau = self.run_camb(pardictbefore)
+        self.clTTEETE_mintau = minus_tau[1:4]
+        plus_tau = self.run_camb(pardictafter)
+        self.clTTEETE_plustau = plus_tau[1:4]
+
+        pardictbefore["tau_reio"] = float(pardict["tau_reio"]) * (
+            1.0 - 2.0 * fracsteptau
+        )
+        pardictafter["tau_reio"] = float(pardict["tau_reio"]) * (
+            1.0 + 2.0 * fracsteptau
+        )
+        minus_tau = self.run_camb(pardictbefore)
+        self.clTTEETE_mintau2 = minus_tau[1:4]
+        plus_tau = self.run_camb(pardictafter)
+        self.clTTEETE_plustau2 = plus_tau[1:4]
 
         self.lminTT = int(pardict["lminTT"])
         self.lmaxTT = int(pardict["lmaxTT"])